f04d54b2ca0c0f520674982ea1119c38762035a5
LukeJwalsh <lukejwalsh1701@gmail.com>
2024-07-02 00:00:47 -0400

    oh i did alot

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// main.js
+// main.js
+document.addEventListener("DOMContentLoaded", () => {
+    const contactNav = document.getElementById("nav-contact");
+    const homeNav = document.getElementById("nav-home");
+    const projectsNav = document.getElementById("nav-projects");
+
+    const homeView = document.getElementById("home-view");
+    const contactView = document.getElementById("contact-view");
+
+    contactNav.addEventListener("click", (event) => {
+        event.preventDefault();
+        homeView.style.display = "none";
+        contactView.style.display = "block";
+        projectsNav.style.display = "none"; // Hide the projects icon
+    });
+
+    homeNav.addEventListener("click", (event) => {
+        event.preventDefault();
+        homeView.style.display = "block";
+        contactView.style.display = "none";
+        projectsNav.style.display = "block"; // Show the projects icon
+        window.scrollTo(0, 0);
+    });
+});
